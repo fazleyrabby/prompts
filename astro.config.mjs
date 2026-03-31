e5668f2727ed8713bda07a736ed9842addb2a648
Md. Fazley Rabbi <fazley111@gmail.com>
2026-03-31 23:14:39 +0600

    deploy: add GitHub Pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  // replace with your GitHub Pages URL
+  site: 'https://fazleyrabby.github.io',
+  base: '/prompts',
   integrations: [tailwind()],
   prefetch: true,
 });
