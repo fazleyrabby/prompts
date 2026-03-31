b629225ef41dc3330bcc93865dc6dc89aa40d9df
Md. Fazley Rabbi <fazley111@gmail.com>
2026-03-31 23:48:48 +0600

    deploy: update for custom subdomain prompts.fazleyrabbi.xyz

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  // replace with your GitHub Pages URL
-  site: 'https://fazleyrabby.github.io',
-  base: '/prompts',
+  // updated for your custom subdomain
+  site: 'https://prompts.fazleyrabbi.xyz',
+  base: '/', 
   integrations: [tailwind()],
   prefetch: true,
 });
